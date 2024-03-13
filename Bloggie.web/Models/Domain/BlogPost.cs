63d2c6e763f48f8336c7e3ecb1a87bb9a0ce5086
jerome <zachariah11jerobert@gmail.com>
2024-03-13 12:05:31 +0530

    Add project files.

---
+++ Bloggie.web/Models/Domain/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿namespace Bloggie.web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid   Id { get; set; }
+        public string Heading { get; set; }
+        public string pageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set;}
+        public bool Visible { get; set; }
+
+    }
+}
